47356d5c583004e95a5ae3772bdfabd2f2257422
dacbac2702 <bac27022002@gmail.com>
2023-12-14 04:02:24 +0700

    Upload files

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello, World!. Vương Đắc Bắc',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
